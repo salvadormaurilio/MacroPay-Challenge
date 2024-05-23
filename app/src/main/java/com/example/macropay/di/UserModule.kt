935ed767596fbdf4a4a55c7f9e6c5ec331bda423
salvadormaurilio <salvadoredgar99@gmail.com>
2024-05-23 17:41:09 -0600

    Add use case and dependencies

---
+++ app/src/main/java/com/example/macropay/di/UserModule.kt
@@ -0,0 +1,23 @@
+package com.example.macropay.di
+
+import com.example.macropay.core.coroutines.CoroutinesDispatchers
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideCoroutinesDispatchers() = CoroutinesDispatchers()
+}
