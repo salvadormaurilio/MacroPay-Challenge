11050f9ce089498ca1a210b7cd444b0ad4bba8e6
salvadormaurilio <salvadoredgar99@gmail.com>
2024-05-23 21:05:12 -0600

    Add Retrofit Services

---
+++ app/src/main/java/com/example/macropay/data/datasource/remote/retrofit/MoviesConstantsRestrofit.kt
@@ -0,0 +1,9 @@
+package com.example.macropay.data.datasource.remote.retrofit
+
+const val AUTHENTICATION_HEADER = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDgyMzkzNDQzODA3NWQ2M2YxZGJkYTQwMjNlNzZmYyIsInN1YiI6IjY1MDBmNzJkNTU0NWNhMDExYmE2N2RkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4QxbpZq9Tj3uzhA8uv2qLNcCA7NIcGBHDzoC4bWv9t8"
+
+const val MOVIE_ID_PATH = "movie_id"
+
+const val MOVIES_ENDPOINT = "3/movie/now_playing?language=es"
+const val MOVIE_DETAIL_ENDPOINT = "3/movie/{$MOVIE_ID_PATH}"
+
---
+++ app/src/main/java/com/example/macropay/data/datasource/remote/retrofit/MoviesServiceRetrofit.kt
@@ -0,0 +1,15 @@
+package com.example.macropay.data.datasource.remote.retrofit
+
+import com.example.macropay.data.datasource.model.MovieResponse
+import com.example.macropay.data.datasource.model.MoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MoviesServiceRetrofit {
+
+    @GET(MOVIES_ENDPOINT)
+    suspend fun fetchMovies(): MoviesResponse
+
+    @GET(MOVIE_DETAIL_ENDPOINT)
+    suspend fun fetchMovieDetail(@Path(MOVIE_ID_PATH) movementId: String): MovieResponse
+}
